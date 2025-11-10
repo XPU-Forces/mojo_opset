ef6d042d991d8c60daf6b9b9b2c7ff39891f67e8
xupan.1205 <xupan.1205@bytedance.com>
2025-11-10 21:36:44 +0800

    [Feat] add general APIs with ref forward and attr for TND bnsd.
    * add general APIs with ref forward and attr for TND/BNSD (Part 1 moe & norm & pos_emb & linear)
    * add general APIs with ref forward and attr for TND/BNSD (Part 2 paged attention)
    * change comments for general APIs
    * Refactor KV layout constants and fix some comments

--- mojo_opset/core/__init__.py
+++ mojo_opset/core/__init__.py
@@ -2,6 +2,9 @@
 All Mojo Operators contained in Mojo Opsets listed here.
 """
 
+# Set of all valid KV layouts for parameter validation (sorted for consistent ordering)
+VALID_KV_LAYOUTS = sorted({"NPU_ND", "NPU_NZ", "AMD_CB"})
+
 """mojo bigop"""
 
 """ mojo activation """
