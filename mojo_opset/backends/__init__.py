98ceacb3f1b0ed74446d502f4a245f893cfaeee8
taochun.chern <taochun.chern@BYTEDANCE.COM>
2025-12-18 20:33:20 +0800

    [TTX] fix ttx support backend & multi-backend dispatch

--- mojo_opset/backends/__init__.py
+++ mojo_opset/backends/__init__.py
@@ -1,2 +1,10 @@
 from .reference import *
-from .ttx import *
+
+from mojo_opset.utils.platform import get_platform
+
+platform = get_platform()
+
+_SUPPORT_TTX_PLATFROM_ = ["npu"]
+
+if platform in _SUPPORT_TTX_PLATFROM_:
+    from .ttx import *
