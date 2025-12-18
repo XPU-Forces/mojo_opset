5cb9a58977c40ebbaf93fceefc13e8930aa5aa42
taochun.chern <taochun.chern@BYTEDANCE.COM>
2025-12-18 20:36:24 +0800

    [TTX] fix code style

--- mojo_opset/backends/__init__.py
+++ mojo_opset/backends/__init__.py
@@ -4,7 +4,7 @@ from mojo_opset.utils.platform import get_platform
 
 platform = get_platform()
 
-_SUPPORT_TTX_PLATFROM_ = ["npu"]
+_SUPPORT_TTX_PLATFROM = ["npu"]
 
-if platform in _SUPPORT_TTX_PLATFROM_:
+if platform in _SUPPORT_TTX_PLATFROM:
     from .ttx import *
