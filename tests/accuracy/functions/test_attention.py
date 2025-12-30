562fec616bedbf0b9c50a160ed70fda63f93d7c9
Zixuan <mazixuan.hpc@BYTEDANCE.COM>
2025-12-30 15:47:15 +0800

    feat: add threshold for attention test.

--- tests/accuracy/functions/test_attention.py
+++ tests/accuracy/functions/test_attention.py
@@ -50,6 +50,29 @@ def generate_test_data(
     return query, key, value, blockwise_diffusion_attn_mask, q_head_num != kv_head_num
 
 
+def compare_tensor(a: torch.Tensor, ref: torch.Tensor, dtype: torch.dtype = None):
+    assert a.shape == ref.shape
+    assert a.dtype == ref.dtype
+    if dtype is None:
+        dtype = a.dtype
+    if dtype == torch.bfloat16:
+        max_atol = 0.1
+        max_rtol = 0.05
+        mean_atol = 0.01
+        mean_rtol = 0.01
+    elif dtype == torch.float32:
+        max_atol = 5e-3
+        max_rtol = 5e-3
+        mean_atol = 1e-4
+        mean_rtol = 1e-4
+    else:
+        print(f"dtype {dtype} is not supported.")
+        assert False
+
+    assert torch.allclose(a, ref, atol=max_atol, rtol=max_rtol)
+    assert torch.mean(torch.abs(ref - a)) < max_atol or torch.mean(torch.abs((ref - a) / (ref + mean_atol))) < mean_rtol
+
+
 @pytest.mark.parametrize(
     "query, key, value, blockwise_diffusion_attn_mask, enable_gqa",
     [
@@ -75,13 +98,13 @@ def test_diffusion_attention_func(monkeypatch, query, key, value, blockwise_diff
         ctx_ref, query, key, value, blockwise_diffusion_attn_mask, 1.0, enable_gqa
     )
 
-    assert torch.allclose(o, o_ref)
+    compare_tensor(o, o_ref)
 
     do = torch.rand_like(o)
-    dq, dk, dv = MojoSdpaFunction.backward(ctx, do)
+    dq, dk, dv, _, _, _ = MojoSdpaFunction.backward(ctx, do)
 
-    dq_ref, dk_ref, dv_ref = MojoSdpaFunction._registry.get("ref").backward(ctx_ref, do)
+    dq_ref, dk_ref, dv_ref, _, _, _ = MojoSdpaFunction._registry.get("ref").backward(ctx_ref, do)
 
-    assert torch.allclose(dq, dq_ref)
-    assert torch.allclose(dk, dk_ref)
-    assert torch.allclose(dv, dv_ref)
+    compare_tensor(dq, dq_ref)
+    compare_tensor(dk, dk_ref)
+    compare_tensor(dv, dv_ref)
