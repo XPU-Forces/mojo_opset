f0e1175cbe68958fe6dbd33629b663effb3494d2
Zixuan <mazixuan.hpc@BYTEDANCE.COM>
2026-01-10 16:45:20 +0800

    fix: change test backend to torch.

--- tests/accuracy/functions/test_position_embedding.py
+++ tests/accuracy/functions/test_position_embedding.py
@@ -1,7 +1,11 @@
 import pytest
 import torch
 
-from tests.utils import auto_switch_platform, bypass_not_implemented, MockFunctionCtx, assert_close
+from tests.utils import MockFunctionCtx
+from tests.utils import assert_close
+from tests.utils import auto_switch_platform
+from tests.utils import bypass_not_implemented
+
 from mojo_opset import MojoRoPEFunction
 
 
@@ -31,7 +35,7 @@ def test_rope_forward_backward_diff(q, k):
     q_rot, k_rot = MojoRoPEFunction.forward(ctx, q, k, cos, sin)
 
     ctx_ref = MockFunctionCtx()
-    q_rot_ref, k_rot_ref = MojoRoPEFunction._registry.get("ref").forward(ctx_ref, q, k, cos, sin)
+    q_rot_ref, k_rot_ref = MojoRoPEFunction._registry.get("torch").forward(ctx_ref, q, k, cos, sin)
 
     assert_close(q_rot, q_rot_ref)
     assert_close(k_rot, k_rot_ref)
@@ -40,6 +44,6 @@ def test_rope_forward_backward_diff(q, k):
     grad_k_out = torch.rand_like(k_rot)
 
     grads = MojoRoPEFunction.backward(ctx, grad_q_out, grad_k_out)
-    grads_ref = MojoRoPEFunction._registry.get("ref").backward(ctx_ref, grad_q_out, grad_k_out)
+    grads_ref = MojoRoPEFunction._registry.get("torch").backward(ctx_ref, grad_q_out, grad_k_out)
 
     assert_close(grads, grads_ref)
