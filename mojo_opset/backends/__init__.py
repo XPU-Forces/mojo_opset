0f8f1b4659f267f002f183a98b3160d0c5b39ca2
mc-zhang <zhangbolun6@huawei.com>
2026-01-22 19:25:33 +0800

    add npu backend

--- mojo_opset/backends/__init__.py
+++ mojo_opset/backends/__init__.py
@@ -1,8 +1,12 @@
+import os
 from mojo_opset.utils.platform import get_platform
 
 platform = get_platform()
 
 _SUPPORT_TTX_PLATFROM = ["npu"]
 
-if platform in _SUPPORT_TTX_PLATFROM:
+if (platform in _SUPPORT_TTX_PLATFROM and 
+    os.getenv("MOJO_USE_TTX_BACKEND", "0") == "1"):
     from .ttx import *
+else:
+    from .torch_npu import *
