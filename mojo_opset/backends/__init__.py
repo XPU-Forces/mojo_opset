c476d721fc056fec26db33da99b33a66581673fd
mc-zhang <zhangbolun6@huawei.com>
2026-01-26 15:53:10 +0800

    add npu_gelu

--- mojo_opset/backends/__init__.py
+++ mojo_opset/backends/__init__.py
@@ -3,9 +3,8 @@ from mojo_opset.utils.platform import get_platform
 
 platform = get_platform()
 
-_SUPPORT_TTX_PLATFROM = ["npu", "ttx", "xpu"]
+_SUPPORT_TTX_PLATFROM = ["npu"]
 
 if platform in _SUPPORT_TTX_PLATFROM:
     from .ttx import *
-    if os.getenv("MOJO_BACKEND", "ttx") == "torch_npu":
-        from .torch_npu import *
+    from .torch_npu import *
