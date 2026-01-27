ad2cb3a2933dc17a4180846975212406f7de2956
yinzijun <yinzijun@bytedance.com>
2026-01-27 15:04:07 +0800

    feat: add top-k-sampling ttx and torch implement

--- tests/perf/test_sampling.py
+++ tests/perf/test_sampling.py
@@ -7,6 +7,9 @@ from tests.utils import bypass_not_implemented
 from mojo_opset import MojoJoinProbRejectSampling
 from mojo_opset import MojoRejectSampling
 from mojo_opset import MojoTopPFilter
+from mojo_opset import MojoTopPSampling
+from mojo_opset import MojoTopKSampling
+
 
 
 @pytest.mark.parametrize(
@@ -25,6 +28,24 @@ def test_topp_filter(logits, topk, topp, min_tokens_to_keep):
 
     perf(lambda: top_p_filter_ref(logits, topp, min_tokens_to_keep, topk))  # noqa: F821
     perf(lambda: top_p_filter(logits, topp, min_tokens_to_keep, topk))  # noqa: F821
+
+
+@pytest.mark.parametrize(
+    "logits, topk, min_tokens_to_keep",
+    [
+        (torch.randn(120, 151936), 1000, 1),
+        (torch.randn(15, 155136), 100, 1),
+        (torch.randn(18, 155136), 100, 1),
+    ],
+)
+@auto_switch_platform(set_perf=True)
+@bypass_not_implemented
+def test_topk_sampling(logits, topk, min_tokens_to_keep):
+    top_k_sampling = MojoTopKSampling(top_k=topk, min_tokens_to_keep=min_tokens_to_keep)
+    top_k_sampling_ref = MojoTopKSampling()._registry.get("torch")(top_k=topk, min_tokens_to_keep=min_tokens_to_keep)
+
+    perf(lambda: top_k_sampling_ref(logits))  # noqa: F821
+    perf(lambda: top_k_sampling(logits))  # noqa: F821
 
 
 @pytest.mark.parametrize(
