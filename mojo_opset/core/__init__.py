ac8dd8428a253eb4484955359656fa1dad076cfd
Florence-Li <lixiaofan357@qq.com>
2026-02-02 20:40:28 +0800

    feat:Added TorchOps: npu_fused_infer_attention_score、npu_swiglu、npu_add_rms_norm、npu_quant_matmul_reduce_sum、npu_grouped_matmul, passes unit tests

--- mojo_opset/core/__init__.py
+++ mojo_opset/core/__init__.py
@@ -41,6 +41,10 @@ from .operators.gemm import MojoGemmAllReduce
 from .operators.gemm import MojoGemmReduceScatter
 from .operators.gemm import MojoGroupGemm
 
+""" matmul """
+from .operators.matmul import MojoGroupedMatmul
+from .operators.matmul import MojoGroupQuantMatmulReduceSum
+
 """ embedding """
 from .operators.embedding import MojoEmbedding
 from .operators.embedding import MojoParallelEmbedding
@@ -98,6 +102,8 @@ __all__ = [
     "MojoOperator",
 
     "MojoGelu",
+    "MojoGroupedMatmul",
+    "MojoGroupQuantMatmulReduceSum",
     "MojoSilu",
     "MojoSwiGLU",
 
