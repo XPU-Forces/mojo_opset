5f7dcd11b818e6d3ceeb18dc9e51503bc7f5ff29
wens <zhaowenshuo.oo@bytedance.com>
2026-03-03 11:53:47 +0800

    misc: rebase.

--- mojo_opset/modeling/config.py
+++ mojo_opset/modeling/config.py
@@ -21,51 +21,84 @@ class MojoModelConfig(BaseModel):
     def __init__(self, **data):
         super().__init__(**data)
 
-    # Preshard config
-    preshard_only: bool = False
-
-    # Deterministic config
-    is_deterministic: bool = False
-
-    # Model definition
     model_name: str = ""
-    model_config: "MojoModelConfig" = None
-    hidden_size: int = None
-    head_dim: int = 128
+
+    hidden_size: int
+    embed_dim: int
+    head_dim: int
+    num_heads: int
+    num_kv_heads: int
+    num_layers: int
+
+    vocab_size: int
+    max_position_embeddings: int
+
     dtype: torch.dtype = torch.bfloat16
 
-    # Runtime settings
+    kv_mirror_layers: List[int] = []
+    kv_mirror_imitated_layers: List[int] = []
+
+    rope_mode: str = ""
+    rope_scale: int
+    rope_percentage: float = 1.0
+
+    has_context_layernorm: bool = True
+    has_k_layernorm: bool = True
+    use_rmsnorm: bool = True
+    residual_post_ln_layers: List[int] = []
+
+    has_attn_bias: bool = False
+    gqa_weights_layout: str = "AABB"
+    q_head_times: int = 1
+
+    moe_expert_num: int = 0
+    moe_topk: int = 0
+    share_expert_num: int = 0
+    moe_ffn_internal_dim: int = 0
+    moe_ffn_has_bias: bool = False
+    is_exp_moe: bool = False
+
+    has_mlp_gate: bool = True
+
+    is_meta: bool = False
+
+    class Config:
+        # Enable support for arbitrary types
+        arbitrary_types_allowed = True
+        extra = "allow"
+
+    @validator("dtype", pre=True)
+    def validate_dtype(cls, value):
+        if isinstance(value, str):
+            if value in dtype_mapping:
+                return dtype_mapping[value]
+            else:
+                raise ValueError(f"unsupported dtype: {value}")
+        return value
+
+
+class MojoRunTimeConfig(BaseModel):
+    def __init__(self, **data):
+        super().__init__(**data)
+
+    preshard_only: bool = False
+
+    is_deterministic: bool = False
+
     use_npu_graph: bool = False
     npu_graph_capture_range: List[int] = []
     use_paged_attention: bool = False
     use_mtp: bool = False
     mtp_draft_recurrent: bool = False
 
-    # Batching and sequence length
     max_batch_size: int = 16
     max_length: int = 2048
     max_total_tokens: int = 0
     max_num_pred_tokens: int = -1
 
-    # Distributed settings
-    world_size: int = 1
-    local_world_size: int = 1
-    global_rank: int = 0
-    local_rank: int = 0
-    tp_size: int = 1
-    dp_size: int = 1
-    tp_rank: int = 0
-    dp_rank: int = 0
-    ep_rank: int = 0
-    tp_group: torch.distributed.ProcessGroup = None
-    dp_group: torch.distributed.ProcessGroup = None
-    ep_group: torch.distributed.ProcessGroup = None
-
-    # Paged attention specific
     num_pages: int = 32
     page_block_size: int = 256
 
-    # Checkpoint and weights
     vanilla_checkpoint_path: str = None
     preshard_checkpoint_path: str = None
 
@@ -82,3 +115,25 @@ class MojoModelConfig(BaseModel):
             else:
                 raise ValueError(f"unsupported dtype: {value}")
         return value
+
+
+class MojoParallelismConfig(BaseModel):
+    dp_size: int = 1
+    pp_size: int = 1
+    ep_size: int = 1
+    tp_size: int = 1
+    dp_rank: int = 1
+    pp_rank: int = 1
+    ep_rank: int = 1
+    tp_rank: int = 1
+    dp_group: list = []
+    pp_group: list = []
+    ep_group: list = []
+    tp_group: list = []
+    world_size: int = 1
+
+
+class MojoConfig(BaseModel):
+    model_config: MojoModelConfig = None
+    parallelism_config: MojoParallelismConfig = None
+    runtime_config: MojoRunTimeConfig = None
