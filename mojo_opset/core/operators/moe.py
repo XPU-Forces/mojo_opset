2b620adbbaca851954f2bb6e37da93c84079687d
zhangjihang <zhangjihang@bytedance.com>
2026-03-06 16:06:31 +0800

    Refactor moe & ep parallel
    - MojoMoE serve like a simgle progress without any parallel implement.
    - Support EP parallel

--- mojo_opset/core/operators/moe.py
+++ mojo_opset/core/operators/moe.py
@@ -13,18 +13,29 @@ class MojoMoE(MojoOperator):
         num_experts,
         top_k,
         hidden_size,
-        intermediate_size,
+        intermediate_size=None,
+        ffn_intermediate_size=None,
         activation: str = "swiglu",
         **kwargs,
     ):
         super().__init__(**kwargs)
+        if activation != "swiglu":
+            raise NotImplementedError(f"MojoMoe: Activation {activation} is not supported.")
+
         for k in ("ep_rank", "ep_size"):
             if k in kwargs:
                 raise ValueError(f"MojoMoE: {k} is not supported; use ParallelStyle to set expert partition.")
 
         # NOTE: in some cases, branches may have different expert num or topk
         self.num_experts = num_experts
-        # self.num_experts_share = num_experts_share
+        if intermediate_size is None:
+            intermediate_size = ffn_intermediate_size
+        if intermediate_size is None:
+            raise ValueError("MojoMoE: intermediate_size must be provided.")
+
+        self.num_experts_per_partion = self.num_experts
+        self.experts_start_idx = 0
+        self.experts_end_idx = self.num_experts
 
         self.top_k = top_k
         self.hidden_size = hidden_size
