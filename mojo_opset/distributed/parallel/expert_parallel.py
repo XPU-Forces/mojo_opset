959f4ab1c9d0dd83f0df7700a2afcd663de36309
chenyifan.42 <chenyifan.42@bytedance.com>
2026-03-12 20:45:51 +0800

    feat: (demo) commit of EP w/ DP

--- mojo_opset/distributed/parallel/expert_parallel.py
+++ mojo_opset/distributed/parallel/expert_parallel.py
@@ -20,37 +20,65 @@ class _MojoEPAllReduceWrapper(nn.Module):
         if torch.distributed.is_available() and torch.distributed.is_initialized():
             y = fc.all_reduce(y, "sum", self.group)
         return y
+    
+class _MojoEPAll2AllWrapper(nn.Module):
+    def __init__(self, inner: nn.Module, group):
+        super().__init__()
+        assert isinstance(inner, MojoMoE)
+        self.inner = inner
+        self.group = group
+
+    def forward(self, x, *args, **kwargs):
+        x = fc.all_gather_tensor_autograd(x, 0, self.group)
+        y = self.inner(x, *args, **kwargs)
+        y = fc.reduce_scatter_tensor_autograd(y, reduceOp="sum", scatter_dim=0, group=self.group)
+        # Step 1: gating on local tokens
+        # top_k_indices, expert_weights = self.inner._gating(x)
+        # Step 2: dispatch local tokens according to router
+        # expert_inputs, pack_gates, pack_index = self.inner._dispatch_ep(x, expert_weights, top_k_indices)
+        
+        # Step 3: compute on local experts
+        # Step 4: combine tokens according to router
+        return y
 
 
 class MojoExpertParallel(ParallelStyle):
-    def __init__(self):
+    def __init__(self, *, replicate_input: bool = True):
         super().__init__()
         self._parallel_style_map = {(MojoMoE): self._partition_fn}
+        self.replicate_input = replicate_input
 
     def _partition_fn(self, name: str, module: nn.Module, device_mesh: DeviceMesh) -> nn.Module:
         ep_size = device_mesh.size()
         ep_rank = device_mesh.get_rank()
 
-        base_num_experts = module.num_experts // ep_size
-        remainder_experts = module.num_experts % ep_size
+        def get_local_num_experts(ep_id: int):
+            base_num_experts = module.num_experts // ep_size
+            remainder_experts = module.num_experts % ep_size
 
-        if ep_rank < remainder_experts:
-            local_num_experts = base_num_experts + 1
-        else:
-            local_num_experts = base_num_experts
-
-        experts_start_idx = base_num_experts * ep_rank + min(ep_rank, remainder_experts)
-        experts_end_idx = experts_start_idx + local_num_experts
+            if ep_id < remainder_experts:
+                local_num_experts = base_num_experts + 1
+            else:
+                local_num_experts = base_num_experts
+            return local_num_experts
+        
+        expert_map = [get_local_num_experts(ep_id) for ep_id in range(ep_size)]
+        
+        experts_start_idx = sum(expert_map[:ep_rank])
+        experts_end_idx = experts_start_idx + expert_map[ep_rank]
 
         fc1_dt = distribute_tensor(module.fc1, device_mesh, [Shard(0)], src_data_rank=self.src_data_rank)
         module.register_parameter("fc1", nn.Parameter(fc1_dt.to_local()))
         fc2_dt = distribute_tensor(module.fc2, device_mesh, [Shard(0)], src_data_rank=self.src_data_rank)
         module.register_parameter("fc2", nn.Parameter(fc2_dt.to_local()))
 
-        module.num_experts_per_partion = local_num_experts
+        module.expert_map = expert_map
         module.experts_start_idx = experts_start_idx
         module.experts_end_idx = experts_end_idx
 
     def _apply(self, module: nn.Module, device_mesh: DeviceMesh) -> nn.Module:
         self._partition_fn("", module, device_mesh)
-        return _MojoEPAllReduceWrapper(module, (device_mesh, 0))
+        if self.replicate_input:
+            return _MojoEPAllReduceWrapper(module, (device_mesh, 0))
+        else:
+            return _MojoEPAll2AllWrapper(module, (device_mesh, 0))
