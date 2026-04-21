434a6f1ec682355b1aa9aafa66af9b640f42417f
chenyifan.42 <chenyifan.42@bytedance.com>
2026-04-21 11:26:53 +0800

    feat: implement quant linear

--- mojo_opset/core/__init__.py
+++ mojo_opset/core/__init__.py
@@ -48,6 +48,7 @@ from .operators.gemm import MojoGroupGemm
 from .operators.gemm import MojoGroupGemm as MojoGroupLinear
 from .operators.gemm import MojoQuantGroupLinearReduceSum
 from .operators.linear import MojoLinear
+from .operators.linear import MojoQuantLinear
 
 """ compute + comm """
 from .operators.compute_with_comm import MojoAllGatherGemm
@@ -174,6 +175,7 @@ __all__ = [
     "MojoStorePagedMLAKVCache",
 
     "MojoLinear",
+    "MojoQuantLinear",
     "MojoGemmDequant",
     "MojoGroupGemm",
     "MojoQuantGroupLinearReduceSum",
