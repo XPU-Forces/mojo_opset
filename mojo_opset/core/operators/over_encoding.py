3717594cee89ade339afe1480edf31a14b4e4641
heke.13 <heke.13@bytedance.com>
2026-04-27 14:28:12 +0800

    [Fix] fix oe weight loading.
    * fix oe weight loading (#261)

--- mojo_opset/core/operators/over_encoding.py
+++ mojo_opset/core/operators/over_encoding.py
@@ -85,6 +85,7 @@ class MojoOverEncodingNGram(MojoOperator):
                 if not isinstance(oe_vocab_sizes, torch.Tensor)
                 else oe_vocab_sizes
             ),
+            persistent=False
         )
         self.register_buffer(
             "oe_grams",
@@ -93,6 +94,7 @@ class MojoOverEncodingNGram(MojoOperator):
                 if not isinstance(oe_grams, torch.Tensor)
                 else oe_grams
             ),
+            persistent=False
         )
         self.register_buffer(
             "oe_vocab_offsets",
@@ -110,12 +112,7 @@ class MojoOverEncodingNGram(MojoOperator):
                 ).to(torch.long),
                 dim=0,
             ),
-        )
-
-        self.register_load_state_dict_post_hook(
-            __make_hook_to_ignore_workspace_buffer__(
-                ("oe_vocab_sizes", "oe_grams", "oe_vocab_offsets")
-            ),
+            persistent=False
         )
 
     def forward(
