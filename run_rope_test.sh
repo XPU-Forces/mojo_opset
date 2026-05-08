2c7759cb2c942a0306f43ba90297d16778151c1a
xupan <xupan.1205@bytedance.com>
2026-05-08 22:50:51 +0800

    drop local rope test helper

--- run_rope_test.sh
+++
@@ -1,4 +0,0 @@
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-cd /data00/xupan/encoder_project/mojo_opset
-export MOJO_BACKEND=torch
-python3 -m pytest mojo_opset/tests/accuracy/operators/test_position_embedding.py -k "vision_rotary_embedding_2d or apply_vision_rope_2d"
