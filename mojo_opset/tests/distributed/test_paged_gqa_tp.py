b9e85098cf295c641c06c72ca98daef16708f8f1
Mayyyybe <145976178+Mayyyybe@users.noreply.github.com>
2026-05-14 14:25:25 +0800

    feat: Ilu/ixformer support quant, rope, kv op. (#299)
    * [ilu/ixformer] add IxformerGroupRMSNorm
    
    * [ilu/ixformer] support IxformerGroupRMSNorm and IxformerApplyRoPE
    
    * [ilu/ixformer] use rms_norm_qk in MojoGroupRMSNorm
    
    * [ilu/ixformer] add IxformerStaticQuant & IxformerDynamicQuant
    
    * [ilu/ixformer] support ixformer StorePagedKVCache
    
    * [ilu/ixformer] update IxformerDynamicQuant
    
    * replace ixf_ops as ixf_f
    
    * [ilu/ixformer] fix IxformerStaticQuant & IxformerDynamicQuant
    
    * misc: refine interface.
    
    * misc: refine tests.
    
    * Update container options in CI workflow
    
    Removed 'network=host' option from container configuration.
    
    * misc: del test.
    
    ---------
    
    Co-authored-by: xiaomei.wang <xiaomei.wang@iluvatar.com>
    Co-authored-by: 江帆 <fan.jiang@iluvatar.com>
    Co-authored-by: xudong.zhao <xudong.zhao@iluvatar.com>
    Co-authored-by: wens <zhaowenshuo.oo@bytedance.com>

--- mojo_opset/tests/distributed/test_paged_gqa_tp.py
+++ mojo_opset/tests/distributed/test_paged_gqa_tp.py
@@ -128,7 +128,7 @@ class SimplePagedKVCache(torch.nn.Module):
             value_states,
             self.k_cache,
             self.v_cache,
-            build_paged_kv_chunk_metadata(
+            chunk_metadata=build_paged_kv_chunk_metadata(
                 self.block_tables[layer_idx],
                 cu_seqlens,
                 current_seq_lens,
